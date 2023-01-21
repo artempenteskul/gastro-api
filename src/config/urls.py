5927680866f6442f12ab271a72e0bf8fc3234762
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-01-21 20:35:48 +0200

    added test view

--- src/config/urls.py
+++ src/config/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls', namespace='users')),
     path('api-auth/', include('rest_framework.urls')),
 ]
