dcc195269c5b2cfed5d21528bc305da3515c93b1
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-01-22 03:15:37 +0200

    removed test view

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 
-from .views import home
-
 
 app_name = 'users'
 
 urlpatterns = [
-    path('', home, name='home')
+
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -1,6 +1,1 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def home(request):
-    return HttpResponse('<h1>Home</h1>')
