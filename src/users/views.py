8a7769e7295cc9f77ef8f6f93c4afadf5bd06db1
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-01-23 23:49:22 +0200

    test api view

--- src/users/views.py
+++ src/users/views.py
@@ -1,1 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+
+
+class AuthView(generics.GenericAPIView):
+    def get(self, request):
+        return Response(data={'message': 'Hello'})
