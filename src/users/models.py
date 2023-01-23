f2a7f3b5b75bb68aa853c8181946af8674b8a716
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-01-24 00:31:16 +0200

    added user manager class

--- src/users/models.py
+++ src/users/models.py
@@ -1,5 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 
 
-class User(models.Model):
-    pass
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password, **kwargs):
+        if not email:
+            raise ValueError('Email should be provided')
+
+        email = self.normalize_email(email)
+        new_user = self.model(email=email, **kwargs)
+        new_user.set_password(password)
+        new_user.save()
+        return new_user
+
+    def create_superuser(self, email, password, **kwargs):
+        kwargs.setdefault('is_staff', True)
+        kwargs.setdefault('is_superuser', True)
+        kwargs.setdefault('is_active', True)
+
+        if not kwargs.get('is_staff'):
+            raise ValueError('Superuser is_staff property should be True')
+
+        if not kwargs.get('is_superuser'):
+            raise ValueError('Superuser is_superuser property should be True')
+
+        if not kwargs.get('is_active'):
+            raise ValueError('Superuser is_active property should be True')
+
+        return self.create_user(email, password, **kwargs)
