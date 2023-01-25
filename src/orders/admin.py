0990e25ad174720a47c04d186f646e6923b7d945
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-01-25 21:37:45 +0200

    updated admin class for order model

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -5,5 +5,11 @@ from .models import Order
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    fields = ('id', 'customer', 'status', ('created_at', 'closed_at'))
     list_display = ('id', 'customer', 'status', 'created_at', 'closed_at')
     list_filter = ('status', 'customer', 'created_at')
+    readonly_fields = ('id', 'customer', 'status', 'created_at', 'closed_at')
+    list_select_related = ('customer',)
+    search_fields = ('id', 'customer')
+    empty_value_display = '-'
+    list_per_page = 50
