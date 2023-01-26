5ea122e45dbd303b7771f1266e4818d2b67e710a
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-01-26 23:46:35 +0200

    fixed admin panel issues for order model

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -5,9 +5,10 @@ from .models import Order
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    fields = ('id', 'customer', 'status', ('created_at', 'closed_at'))
+    fields = ('id', 'customer', 'status', 'closed_at')
     list_display = ('id', 'customer', 'status', 'created_at', 'closed_at')
     list_filter = ('status', 'customer', 'created_at')
+    readonly_fields = ('id', 'created_at',)
     list_select_related = ('customer',)
     search_fields = ('id', 'customer')
     empty_value_display = '-'
