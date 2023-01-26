e15ea753dca22e868a839693476d1d5bd26b2946
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-01-26 23:23:51 +0200

    modified user admin ui

--- src/users/admin.py
+++ src/users/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 from .models import User
 
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'username', 'email', 'phone')
+    list_display_links = ('username', 'email', 'phone')
+    search_fields = ('first_name', 'last_name', 'username', 'email', 'phone')
+    empty_value_display = '-'
+    list_per_page = 50
